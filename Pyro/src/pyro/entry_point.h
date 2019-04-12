9dccf4b5f81c91437246b0ff15baf127f482b8c9
Pietro Lazzarotto <pietro.lazz@gmail.com>
2019-04-12 22:25:08 +0100

    added comments

--- Pyro/src/pyro/entry_point.h
+++ Pyro/src/pyro/entry_point.h
@@ -1,7 +1,14 @@
 #pragma once
 
+//=============================================================================
+// Specifies the main entry point of the application regardless of the 
+// platform in which the project will be built onto.
+// The engine is supposed to handle the pain of creating it because of this.
+//=============================================================================
+
 #ifdef PYRO_PLATFORM_WIN
 
+/// \brief Externally declared in the executable project. 
 extern pyro::application* pyro::create_application();
 
 int main(int argc, char** argv)
