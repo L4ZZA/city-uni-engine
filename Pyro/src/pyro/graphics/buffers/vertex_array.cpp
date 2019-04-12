33711dc2379c5774832efd85bf7894ff13c25fe6
Pietro Lazzarotto <pietro.lazz@gmail.com>
2019-04-12 22:17:33 +0100

    buffers abstractions placeholders
    still to fully implement those!

---
+++ Pyro/src/pyro/graphics/buffers/vertex_array.cpp
@@ -0,0 +1,23 @@
+﻿#include "pyro_pch.h"
+#include "vertex_array.h"
+#include "glad/glad.h"
+
+pyro::vertex_array::vertex_array()
+{
+    glGenVertexArrays(1, &m_id);
+}
+
+pyro::vertex_array::~vertex_array()
+{
+    glDeleteVertexArrays(1, &m_id);
+}
+
+void pyro::vertex_array::bind() const
+{
+    glBindVertexArray(m_id);
+}
+
+void pyro::vertex_array::unbind() const
+{
+    glBindVertexArray(0);
+}
