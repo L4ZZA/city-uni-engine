5d1e560470e523d718ce8a16e3caefa2b8702244
Pietro Lazzarotto <pietro.lazz@gmail.com>
2019-04-13 13:59:50 +0100

    Reminder

--- Pyro/src/pyro/graphics/shaders/static_shader.cpp
+++ Pyro/src/pyro/graphics/shaders/static_shader.cpp
@@ -1,6 +1,8 @@
 ﻿#include "pyro_pch.h"
 #include "static_shader.h"
 
+// Keep in mind these paths should be relative to the sandbox project, until I fix it.
+
 const std::string pyro::static_shader::s_vertex_file = "res/shaders/vertex.shader";
 const std::string pyro::static_shader::s_fragment_file = "res/shaders/fragment.shader";
 
