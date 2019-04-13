aa7a6db0bc108c778b4c144231b3c947e89242bb
Pietro Lazzarotto <pietro.lazz@gmail.com>
2019-04-13 14:26:30 +0100

    Fixed warning

--- Pyro/src/pyro/graphics/raw_model.cpp
+++ Pyro/src/pyro/graphics/raw_model.cpp
@@ -1,15 +1,15 @@
 ﻿#include "pyro_pch.h"
 #include "raw_model.h"
 
-pyro::raw_model::raw_model(unsigned int vao_id, int vertex_count)
+pyro::raw_model::raw_model(unsigned int vao_id, size_t vertex_count)
 {
     create(vao_id, vertex_count);
 }
 
-void pyro::raw_model::create(unsigned int vao_id, int vertex_count)
+void pyro::raw_model::create(unsigned int vao_id, size_t vertex_count)
 {
     m_vao_id = vao_id;
-    m_vertex_count = vertex_count;
+    m_vertex_count = static_cast<int>(vertex_count);
 }
 
 unsigned pyro::raw_model::vao_id() const
--- Pyro/src/pyro/graphics/raw_model.h
+++ Pyro/src/pyro/graphics/raw_model.h
@@ -6,8 +6,8 @@ namespace pyro
     {
     public:
         raw_model() = default;
-        raw_model(unsigned int vao_id, int vertex_count);
-        void create(unsigned int vao_id, int vertex_count);
+        raw_model(unsigned int vao_id, size_t vertex_count);
+        void create(unsigned int vao_id, size_t vertex_count);
 
         unsigned int vao_id() const;
         int vertex_count() const;
