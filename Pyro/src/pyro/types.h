c3dcf9dcf31315ab3a14097c48d6227ccf703c66
Pietro Lazzarotto <pietro.lazz@gmail.com>
2019-04-14 11:53:21 +0100

    Extended pch and added types header

---
+++ Pyro/src/pyro/types.h
@@ -0,0 +1,6 @@
+#pragma once
+
+// ============== TYPE DEFINITIONS ==============
+
+template<typename T> using hash_set = std::unordered_set<T>;
+template<typename K, typename V> using hash_map = std::unordered_map<K, V>;
--- Pyro/src/pyro_pch.h
+++ Pyro/src/pyro_pch.h
@@ -1,17 +1,23 @@
 ﻿#pragma once
 
-#include <iostream>
 #include <memory>
-#include <utility>
 #include <algorithm>
 #include <functional>
+#include <iostream>
 #include <string>
 #include <sstream>
+
+#include <array>
 #include <vector>
+#include <list>
+#include <stack>
+#include <map>
 #include <unordered_map>
 #include <unordered_set>
 
+
 #include "pyro/logger.h"
+#include "pyro/core.h"
 
 #ifdef PYRO_PLATFORM_WIN
     #include <Windows.h>
