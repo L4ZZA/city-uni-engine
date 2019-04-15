bd5838a5f0e9f80210b2f953e3a781a3b7d310ae
Pietro Lazzarotto <pietro.lazz@gmail.com>
2019-04-15 17:25:49 +0100

    completed obj loader

--- Pyro/src/pyro/graphics/obj_loader.h
+++ Pyro/src/pyro/graphics/obj_loader.h
@@ -26,11 +26,46 @@ namespace pyro
 			for(const auto& comp : components)
 			{
 				CORE_ASSERT(comp.empty(), "Should not be empty.");
-				T actual = atof(comp.c_str());
+				auto actual = atof(comp.c_str());
 				T c = static_cast<T>(actual);
 				comps.push_back(c);
 			}
 			return comps;
+		}
+		template <typename T>
+		static T parse(const std::string &component)
+		{
+			CORE_ASSERT(component.empty(), "Should not be empty.");
+			auto actual = atof(component.c_str());
+			return static_cast<T>(actual);
+		}
+
+		/// \brief Goes through each triad of vertices that specifies it and 
+		/// composes the textures and normals array so that the values are 
+		/// in the right order.
+		static void process_vertex(const std::vector<std::string> &components,
+			std::vector<unsigned int> &indices,
+			std::vector<glm::vec2> &tex_coords,
+			std::vector<glm::vec3> &normals,
+			std::vector<float> &textures_array,
+			std::vector<float> &normals_array
+		)
+		{
+			// the first element of the triad is the vertex index 
+			// (-1 because the obj starts at 1 and we start at 0)
+			auto current_vertex_index = parse<unsigned int>(components[0]) - 1;
+			indices.push_back(current_vertex_index);
+
+			// the second is the index of the texture in the texture list (aka our container)
+			glm::vec2 current_tex = tex_coords[parse<int>(components[1]) - 1];
+			textures_array[current_vertex_index * 2 + 0] = current_tex.x;
+			textures_array[current_vertex_index * 2 + 1] = 1.f - current_tex.y;// blender starts from the bottom left
+
+			// the second is the index of the normal in the normals list (aka our container)
+			glm::vec3 current_normal = normals[parse<int>(components[2]) - 1];
+			normals_array[current_vertex_index * 3 + 0] = current_normal.x;
+			normals_array[current_vertex_index * 3 + 1] = current_normal.y;
+			normals_array[current_vertex_index * 3 + 2] = current_normal.z;
 		}
 
 		static raw_model load_obj(const std::string &file_name)
@@ -44,6 +79,9 @@ namespace pyro
 			std::vector<glm::vec3> normals;
 			std::vector<unsigned int> indices;
 
+			std::vector<float> textures_array;
+			std::vector<float> normals_array;
+
 			std::string line;
 			std::stringstream ss;
 			while(getline(stream, line))
@@ -56,7 +94,7 @@ namespace pyro
 					// parse components to floats
 					auto f_comps = parse<float>(components);
 					// compose vertex
-					glm::vec3 vertex{f_comps[0],f_comps[1],f_comps[2]};
+					glm::vec3 vertex{ f_comps[0],f_comps[1],f_comps[2] };
 					// add to container
 					vertices.push_back(vertex);
 				}
@@ -68,7 +106,7 @@ namespace pyro
 					// parse components to floats
 					auto f_comps = parse<float>(components);
 					// compose tex coord
-					glm::vec2 coord{f_comps[0],f_comps[1]};
+					glm::vec2 coord{ f_comps[0],f_comps[1] };
 					// add to container
 					tex_coords.push_back(coord);
 				}
@@ -80,19 +118,47 @@ namespace pyro
 					// parse components to floats
 					auto f_comps = parse<float>(components);
 					// compose normal
-					glm::vec3 normal{f_comps[0],f_comps[1],f_comps[2]};
+					glm::vec3 normal{ f_comps[0],f_comps[1],f_comps[2] };
 					// add to container
 					normals.push_back(normal);
 				}
 				// face
+				static bool first = true;
 				if(line.compare(0, 2, "f ") == 0)
 				{
-					// TODO: see if I need to convert all containers to float vectors
-					break;
+					if(first)
+					{
+						textures_array = std::vector<float>(vertices.size() * 2);
+						normals_array  = std::vector<float>(vertices.size() * 3);
+						first = false;
+					}
+					// split from the first non-blank char after the identifier
+					std::vector<std::string> these_vertices = split(line.substr(2), ' ');
+					std::vector<std::string> vertex1 = split(these_vertices[0], '/');
+					std::vector<std::string> vertex2 = split(these_vertices[1], '/');
+					std::vector<std::string> vertex3 = split(these_vertices[2], '/');
+
+					process_vertex(vertex1, indices, tex_coords, normals, textures_array, normals_array);
+					process_vertex(vertex2, indices, tex_coords, normals, textures_array, normals_array);
+					process_vertex(vertex3, indices, tex_coords, normals, textures_array, normals_array);
+
 				}
 			}
 
-			return raw_model();
+			// convert the vertices list into float array
+			std::vector<float> vertices_array;
+
+			// convert the indices list into int array
+
+			int index = 0;
+			for(auto vec : vertices)
+			{
+				vertices_array.push_back(vec.x);
+				vertices_array.push_back(vec.y);
+				vertices_array.push_back(vec.z);
+			}
+
+			return loader::load_model(vertices_array, textures_array, indices);
 		}
 	};
 }
