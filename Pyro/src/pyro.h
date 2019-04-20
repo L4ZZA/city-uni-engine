c88d77285ef1c37720a42e3065f31351b2a4fc64
L4ZZA <pietro.lazz@gmail.com>
2019-04-20 14:36:22 +0100

    Updated pyro.h so that we can use all classes when including it

--- Pyro/src/pyro.h
+++ Pyro/src/pyro.h
@@ -1,16 +1,27 @@
 #pragma once
 
 // For whoever wants to use Pyro library
+#include "glm/glm.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+#include "glm/gtc/type_ptr.hpp"
 
+#include "pyro/defs.h"
+#include "pyro/core.h"
 #include "pyro/application.h"
 #include "pyro/layer.h"
 #include "pyro/logger.h"
 
-#include "pyro/input.h"
+#include "pyro/events/event.h"
+#include "pyro/events/application_event.h"
+#include "pyro/events/key_event.h"
+#include "pyro/events/mouse_event.h"
+
 #include "pyro/key_codes.h"
 #include "pyro/mouse_buttons_codes.h"
+#include "pyro/input.h"
 
-#include "pyro/imgui/imgui_layer.h"
+#include "pyro/graphics/shader.h"
+#include "pyro/graphics/model.h"
 
 // --------- Entry Point ---------------
 #include "pyro/entry_point.h"
