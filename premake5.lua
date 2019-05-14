5bf3f337e3b6ffcbbd17a5492ef3062b287d0872
L4ZZA <pietro.lazz@gmail.com>
2019-05-14 12:21:41 +0100

    Changed premake script to add .editorconfig

--- premake5.lua
+++ premake5.lua
@@ -1,3 +1,24 @@
+-- Implement the workspace files command for solution-scope files
+require('vstudio')
+premake.api.register {
+	name = "workspace_files",
+	scope = "workspace",
+	kind = "list:string",
+}
+
+premake.override(premake.vstudio.sln2005, "projects", function(base, wks)
+	if wks.workspace_files and #wks.workspace_files > 0 then
+		premake.push('Project("{2150E333-8FDC-42A3-9474-1A3956D46DE8}") = "configs", "configs", "{' .. os.uuid("configs:"..wks.name) .. '}"')
+		premake.push("ProjectSection(SolutionItems) = preProject")
+		for _, path in ipairs(wks.workspace_files) do
+			premake.w(path.." = "..path)
+		end
+		premake.pop("EndProjectSection")
+		premake.pop("EndProject")
+	end
+	base(wks)
+end)
+
 -- workspace is the solution
 workspace "engine"
     architecture "x64"
@@ -10,6 +31,11 @@ workspace "engine"
         "Dist" -- distribution build
     }
 
+	workspace_files 
+	{
+		".editorconfig"
+    }
+    
 -- example: debug-win-x64
 outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
 
