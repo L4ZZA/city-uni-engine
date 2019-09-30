3333803041c902dcab3de118a343f8b4b832d03c
Pietro Lazzarotto <pietro.lazz@gmail.com>
2019-09-30 14:08:57 +0100

    moved freetype and added premake definitions

---
+++ engine/external/freetype/premake5.lua
@@ -0,0 +1,30 @@
+project "freetype"
+  kind "StaticLib"
+  language "C++"
+
+  targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+  objdir ("inter/" .. outputdir .. "/%{prj.name}")
+
+  files {
+    "%{prj.name}/**.h"
+  }
+
+  filter "system:windows"
+    systemversion "latest"
+    cppdialect "C++17"
+    staticruntime "On"
+
+  filter "configurations:Debug"
+      defines "PYRO_DEBUG"
+      runtime "Debug"
+      symbols "on"
+
+  filter "configurations:Release"
+      defines "PYRO_RELEASE"
+      runtime "Release"
+      optimize "on"
+
+  filter "configurations:Dist"
+      defines "PYRO_DIST"
+      runtime "Release"
+      optimize "on"
